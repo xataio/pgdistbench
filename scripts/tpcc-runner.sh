e0a38a2eae607f31f98e649f332715d362753f3e
Steffen Siering <steffen@xata.io>
2025-07-15 11:49:56 +0200

    Fix scripts csv header (#5)

--- scripts/tpcc-runner.sh
+++ scripts/tpcc-runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 BENCH_SCRIPT_HOME=${BENCH_SCRIPT_HOME:-"/opt/sysbench-tpcc"}
 
 # This script prints a CSV header, runs the sysbench-tpcc tool,
@@ -7,10 +9,10 @@ BENCH_SCRIPT_HOME=${BENCH_SCRIPT_HOME:-"/opt/sysbench-tpcc"}
 # It passes all its arguments directly to the sysbench command.
 
 # Print the CSV header
-echo "time,threads,tps,qps,lat_min,lat_avg,lat_max,lat_99th,errors,reconnects"
+echo "time,threads,tps,qps,qps_read,qps_write,qps_other,lat_99th,errors,reconnects"
 
 # Change to the script directory
 cd "$BENCH_SCRIPT_HOME"
 
 # Execute the tpcc.lua script with all arguments, and filter the output
-./tpcc.lua --histogram=off --percentile=99 --db-driver=pgsql --report_csv=yes --verbosity=0 "$@" run | grep -v "DB SCHEMA public" 
+./tpcc.lua --histogram=off --percentile=99 --db-driver=pgsql --report_csv=yes --verbosity=0 $@ run | grep -v "DB SCHEMA public"
